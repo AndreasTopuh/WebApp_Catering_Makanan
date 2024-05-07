5c4db9d453e019ba5eddbc6397cd3fc5e0784be1
jck-y <s2200582@student.unklab.ac.id>
2024-05-07 09:38:57 +0800

    adding delete, put, post endpoint

--- src/itempesanan/itemPesanan.controller.js
+++ src/itempesanan/itemPesanan.controller.js
@@ -1,10 +1,33 @@
-const { getAllItemsPesanan } = require("./itemPesanan.service");
-
+const {
+  getAllItemsPesanan,
+  createNewItemPesanan,
+  updateItemPesanan,
+  deleteItemPesanan,
+} = require("./itemPesanan.service");
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   const itemsPesanan = await getAllItemsPesanan();
   res.json(itemsPesanan);
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const newItemPesanan = await createNewItemPesanan(req.body);
+    res.json(newItemPesanan);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const updatedItemPesanan = await updateItemPesanan(req.params.id, req.body);
+  res.json(updatedItemPesanan);
+});
+
+router.delete("/:id", async (req, res) => {
+  const deletedItemPesanan = await deleteItemPesanan(req.params.id);
+  res.json(deletedItemPesanan);
 });
 
 module.exports = router;
--- src/pelanggan/pelanggan.repository.js
+++ src/pelanggan/pelanggan.repository.js
@@ -5,6 +5,33 @@ const findCustomers = async () => {
   return customers;
 };
 
+const findCustomerById = async (id) => {
+  const customer = await prisma.Pelanggan.findUnique({ where: { id } });
+  return customer;
+};
+
+const createCustomer = async (data) => {
+  const newCustomer = await prisma.Pelanggan.create({ data });
+  return newCustomer;
+};
+
+const updateCustomer = async (id, data) => {
+  const updatedCustomer = await prisma.Pelanggan.update({
+    where: { id },
+    data,
+  });
+  return updatedCustomer;
+};
+
+const deleteCustomer = async (id) => {
+  const deletedCustomer = await prisma.Pelanggan.delete({ where: { id } });
+  return deletedCustomer;
+};
+
 module.exports = {
   findCustomers,
+  findCustomerById,
+  createCustomer,
+  updateCustomer,
+  deleteCustomer,
 };
